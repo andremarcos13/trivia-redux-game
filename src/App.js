5d62be84f04388b19d343a2bc8a3eb1dc797dc75
CaioBGalvao <caio_bgalvao@hotmail.com>
2022-05-24 17:18:10 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <Switch>
         <Route path="/" exact component={ Login } />
         <Route path="/game" exact component={ Game } />
+        <Route path="/settings" exact component={ Settings } />
       </Switch>
     );
   }
---
+++ src/pages/Settings.js
@@ -0,0 +1,26 @@
+// import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+export default class Game extends Component {
+  // static propTypes = {
+  //   second: third
+  // }
+
+  render() {
+    return (
+      <h1
+        data-testid="settings-title"
+      >
+        Settings
+
+      </h1>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => ({})
+
+// const mapDispatchToProps = {}
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Game)
