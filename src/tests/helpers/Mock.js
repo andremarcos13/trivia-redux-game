1583e37fd51db84e7e709c96439810308e4eb15b
Andre Cesar <andrmgc@gmail.com>
2022-06-06 16:46:07 -0300

    realiza testes da pagina feedback

---
+++ src/tests/helpers/Mock.js
@@ -0,0 +1,23 @@
+export const player2corrects={
+    player: {
+      name: '123',
+      assertions: 2,
+      score: 22,
+      gravatarEmail: 'teste@teste.com',
+    }
+  }
+
+  export const player4corrects={
+    player: {
+      name: 'teste',
+      assertions: 4,
+      score: 387,
+      gravatarEmail: 'teste@teste.com',
+    }
+  }
+
+  export const mockDoLocalStorage = [{
+    name: "Teste 1",
+    playerPhoto: "https://www.gravatar.com/avatar/ce11fce876c93ed5d2a72da660496473",
+    score: 33,
+  }]
